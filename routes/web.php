77d4a89a8067ef49ec9f850d69db9e75f0021e18
nurulzulpia <nurulzulpiarahmawati@gmail.com>
2026-06-04 15:45:03 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Middleware\EnsureTeamMembership;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::view('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::prefix('{current_team}')
+    ->middleware(['auth', 'verified', EnsureTeamMembership::class])
+    ->group(function () {
+        Route::view('dashboard', 'dashboard')->name('dashboard');
+    });
+
+Route::middleware(['auth'])->group(function () {
+    Route::livewire('invitations/{invitation}/accept', 'pages::teams.accept-invitation')->name('invitations.accept');
+});
+
+Route::livewire('/categories', 'pages::category.index')
+    ->middleware(['auth'])
+    ->name('category.index');
+require __DIR__.'/settings.php';

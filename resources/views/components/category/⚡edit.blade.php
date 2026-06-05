99a030cdb83c4ca0cd3a53085295289bdcf09c01
nurulzulpia <nurulzulpiarahmawati@gmail.com>
2026-06-05 23:22:03 +0800

    tambahan

--- resources/views/components/category/⚡edit.blade.php
+++ resources/views/components/category/⚡edit.blade.php
@@ -30,10 +30,27 @@ new class extends Component
         $this->resetValidation();
         $this->form->reset();
     }
+
+    #[On('confirm-delete')]
+    public function confirmDelete($id)
+    {
+        $category = Category::find($id);
+        $this->form->setCategory($category);
+        Flux::modal('delete-category')->show();
+    }
+
+    public function deleteCategory() {
+        $this->form->category->delete();
+        Flux::modal('delete-category')->close();
+        session()->flash('success', 'Category deleted successfully');
+        $this->redirectRoute('category.index', navigate: true);
+    }
+
 };
 ?>
 
 <div>
+    {{-- modal untuk edit --}} 
     <flux:modal 
         name="edit-category" 
         class="md:w-150" 
@@ -85,5 +102,34 @@ new class extends Component
 
         </form>
     </flux:modal>
- 
+
+    {{-- delete modal --}}
+
+    <flux:modal 
+        name="delete-category" 
+        class="md:w-150" 
+        x-on:close="$wire.resetForm()" 
+    >
+        <form class="space-y-8" wire:submit.prevent="deleteCategory">
+            {{-- header --}}
+            <div class="space-y-2">
+                <flux:heading size="lg" class="text-zinc-900 dark:text-white">
+                    Delete Category
+                </flux:heading>
+                <flux:text class="text-zinc-500 dark:text-zinc-400">
+                    this action cannot be undone
+                </flux:text>
+            </div>
+
+            {{-- footer --}}
+            <div class="flex items-center justify-end gap-3 pt-4 border-t border-zinc-200 dark:border-zinc-800">
+                <flux:modal.close>
+                    <flux:button variant="outline" color="neutral">Cancel</flux:button>
+                </flux:modal.close>
+                <flux:button variant="primary" color="danger" type="submit">Delete</flux:button>
+            </div>
+                
+
+        </form>
+    </flux:modal>
 </div>
